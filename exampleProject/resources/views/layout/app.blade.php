71c5866fc26a22aaf2517bd6efa8d1047bf34802
Buthmika <buthmikamenushan@gmail.com>
2025-05-04 11:39:00 +0530

    Update app.blade.php

--- exampleProject/resources/views/layout/app.blade.php
+++ exampleProject/resources/views/layout/app.blade.php
@@ -3,9 +3,12 @@
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
+      <title>ToDo</title>
+      @include('libraries.style')
 </head>
 <body>
-      
+    @include('components.nav')
+    @yield('content')
+    @include('libraries.script')
 </body>
 </html>
