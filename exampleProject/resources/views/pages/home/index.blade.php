4bedfa79731039eba0c98d9a40ed6e06bc38522d
Buthmika <buthmikamenushan@gmail.com>
2025-05-04 11:39:25 +0530

    Update index.blade.php

--- exampleProject/resources/views/pages/home/index.blade.php
+++ exampleProject/resources/views/pages/home/index.blade.php
@@ -1,1 +1,22 @@
-<h1>buthmika</h1>
+@extends('layout.app')
+
+@section('content')
+
+<div class="container">
+    <div class="title">
+        <h1>Home</h1>
+    </div>
+</div>
+
+
+@endsection
+@push('css')
+    <style>
+        .title{
+        padding-top: 60px;
+        padding-left: 450px;
+    }
+
+    </style>
+@endpush
+
