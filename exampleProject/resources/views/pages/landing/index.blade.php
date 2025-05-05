c4f3dc6fc582b3ea3675e86b31b869dabd4de521
Buthmika <buthmikamenushan@gmail.com>
2025-05-05 09:19:13 +0530

    Update index.blade.php

--- exampleProject/resources/views/pages/landing/index.blade.php
+++ exampleProject/resources/views/pages/landing/index.blade.php
@@ -1,5 +1,28 @@
 @extends('layout.app')
 @section('content')
- 
+<div class="container">
+    <div class="form">
+        <div class="task">
+            <input type="text" class="form-control" placeholder="Add a new task">
+        </div>
+        <div class="submitButton">
+            <button type="submit" class="btn btn-primary">Add Task</button>
+        </div>
+    </div>
+</div>
 
 @endsection
+@push('css')
+    <style>
+       .task .form-control{
+        width: 600px;
+       }
+       .form{
+        display: flex;
+        gap: 30px;
+        padding-top: 200px;
+        padding-left: 180px;
+       }
+
+    </style>
+@endpush
