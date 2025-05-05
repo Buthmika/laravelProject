1296388bf3968782b5f89042daf9f1a4f4878bcd
Buthmika <buthmikamenushan@gmail.com>
2025-05-05 09:49:16 +0530

    Update web.php

--- exampleProject/routes/web.php
+++ exampleProject/routes/web.php
@@ -5,6 +5,11 @@ use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\LandingController;
 
 Route::get('/',[HomePageController::class,'index'])->name('home');
-Route::get('/landing',[LandingController::class,'index'])->name('landing');
+Route::prefix('/landing')->group (function() {
+    Route::get('/',[LandingController::class,'index'])->name('landing');
+    Route::post('/store',[LandingController::class,'store'])->name('landing.store');
+
+
+});
 
 
