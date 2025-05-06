f3a5f9ecc4511747654df8c4203c301225dc3a23
Buthmika <buthmikamenushan@gmail.com>
2025-05-06 17:51:51 +0530

    Update LandingController.php

--- exampleProject/app/Http/Controllers/LandingController.php
+++ exampleProject/app/Http/Controllers/LandingController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class LandingController extends Controller
 {
+    protected $task;
+    public function __construct()
+    {
+        $this->task = new Todo();
+    }
     public function index()
     {
         return view('pages.landing.index');
     }
+    public function store(Request $request){
+        $this->task->create($request->all());
+    }
+
+
+
 }
